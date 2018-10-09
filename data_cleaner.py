49e84785558d1687e6f3d7ca79bd89cbd3247703
Timo <timo.haario@phz.fi>
2018-10-09 16:05:17 +0300

    removed extra stuff

--- data_cleaner.py
+++
@@ -1,9 +0,0 @@
-import csv
-import re
-
-with open('Data/airbnb_listings.csv', 'r') as csvfile:
-    rdr= csv.reader(csvfile)
-    for r in rdr:
-        with open("Data/listings_separated/"+re.sub('\W+','', str(r[4])), "w") as result:
-            wtr = csv.writer(result)
-            wtr.writerow((r[5], r[6], r[7], r[9], r[11], r[14]))
--- main.py
+++
@@ -1,7 +0,0 @@
-import csv
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use('ggplot')
-import numpy as np
