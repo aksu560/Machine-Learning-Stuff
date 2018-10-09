8088fe63e732de51cc8505d2cb576fba93f88ca8
Timo <timo.haario@phz.fi>
2018-10-09 15:12:25 +0300

    Things and stuff

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,8 +1,9 @@
 import csv
+import re
 
 with open('Data/airbnb_listings.csv', 'r') as csvfile:
     rdr= csv.reader(csvfile)
-    with open('Data/airbnb_listings_clean.csv', 'w') as result:
-        wtr= csv.writer(result)
-        for r in rdr:
-            wtr.writerow((r[4], r[5], r[6], r[7], r[9], r[11], r[14]))
+    for r in rdr:
+        with open("Data/listings_separated/"+re.sub('\W+','', str(r[4])), "w") as result:
+            wtr = csv.writer(result)
+            wtr.writerow((r[5], r[6], r[7], r[9], r[11], r[14]))
